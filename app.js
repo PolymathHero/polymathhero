bdce64a67176d6fdc01eafc5101f50a284036306
bu9loo7 <32558483+bu9loo7@users.noreply.github.com>
2023-03-21 20:53:20 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Select the header element
+const header = document.querySelector('.header');
+
+// Add a click event listener to the header element
+header.addEventListener('click', () => {
+    alert('Header clicked!');
+});
