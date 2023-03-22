1db7add18ddc149072dcefbe3678f2355d6aa249
bu9loo7 <32558483+bu9loo7@users.noreply.github.com>
2023-03-22 22:03:49 +0300

    Create sturm-scripts.js

---
+++ sturm-scripts.js
@@ -0,0 +1,14 @@
+```javascript
+// Simple animation to fade in the title
+document.addEventListener('DOMContentLoaded', function() {
+  const title = document.getElementById('animated-title');
+  let opacity = 0;
+  const fadeIn = setInterval(function() {
+    if (opacity >= 1) {
+      clearInterval(fadeIn);
+    } else {
+      opacity += 0.1;
+      title.style.opacity = opacity;
+    }
+  }, 100);
+});
