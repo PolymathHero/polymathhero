a3a10d6ddcb4967afab5ad501129611980a149a1
bu9loo7 <32558483+bu9loo7@users.noreply.github.com>
2023-03-22 20:40:13 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-        $('header.sticky-nav').addClass('nav-bg');
-    } else {
-        $('header.sticky-nav').removeClass('nav-bg');
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const productImage = document.querySelector('.product-image');
+  const productInfo = document.querySelector('.product-info');
+
+  // Animation for product image
+  anime({
+    targets: productImage,
+    translateY: [-30, 0],
+    opacity: [0, 1],
+    duration: 1000,
+    easing: 'easeOutCubic',
+  });
+
+  // Animation for product info
+  anime({
+    targets: productInfo,
+    translateY: [30, 0],
+    opacity: [0, 1],
+    duration: 1000,
+    easing: 'easeOutCubic',
+    delay: 500,
+  });
 });
