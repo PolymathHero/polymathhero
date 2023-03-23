23d60ea1db795124cfa539768c4990d153cc7f6e
bu9loo7 <32558483+bu9loo7@users.noreply.github.com>
2023-03-23 03:37:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-// Select the header element
-const header = document.querySelector('.header');
+document.addEventListener("DOMContentLoaded", () => {
+  const quoteText = document.querySelector(".quote-text");
+  const quoteAuthor = document.querySelector(".quote-author");
+  const quoteButton = document.querySelector(".quote-button");
+  const twitterShare = document.querySelector(".twitter-share");
 
-// Add a click event listener to the header element
-header.addEventListener('click', () => {
-    alert('Header clicked!');
+  function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    quoteText.textContent = quote.text;
+    quoteAuthor.textContent = `– ${quote.author}`;
+  }
+
+  function updateTwitterShare() {
+    const quoteString = encodeURIComponent(`"${quoteText.textContent}" ${quoteAuthor.textContent}`);
+    twitterShare.href = `https://twitter.com/intent/tweet?text=${quoteString}`;
+  }
+
+  quoteButton.addEventListener("click", () => {
+    generateQuote();
+    updateTwitterShare();
+  });
+
+  // Generate the initial quote
+  generateQuote();
+  updateTwitterShare();
 });
