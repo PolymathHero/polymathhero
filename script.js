b6dba0d59473d8a5fc1e31f34c0c56524c8ac607
Polymath Hero <32558483+PolymathHero@users.noreply.github.com>
2023-12-20 20:38:35 +0300

    a

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // BEGIN SCRIPT TO CHANGE HEADER ON SCROLL
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("myHeader");
-    var headerIcons = document.querySelectorAll(".header-icon-img");
+    var headerIcons = document.querySelectorAll(".header-icon-svg");
+    let headerShoppingBagCounter = document.querySelector('.header-shopping-bag-quantity-notification-span');
 
     window.onscroll = function () {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -9,22 +10,25 @@ document.addEventListener("DOMContentLoaded", function () {
             headerIcons.forEach(function (icon) {
                 icon.classList.add("scrolled");
             });
+            headerShoppingBagCounter.classList.add('scrolled');
         } else {
             header.classList.remove("scrolled");
             headerIcons.forEach(function (icon) {
                 icon.classList.remove("scrolled");
             });
+            headerShoppingBagCounter.classList.remove('scrolled');
         }
     };
 });
 // END SCRIPT TO CHANGE HEADER ON SCROLL
 
-// BEGIN HEADER APPEARANCE ON HOVER
+// BEGIN HEADER APPEARANCE ON HOVER on computers
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("myHeader");
     var anchorElements = header.getElementsByTagName("a");
     var barElements = document.querySelectorAll(".bar");
-    var headerIcons = document.querySelectorAll(".header-icon-img");
+    var headerIcons = document.querySelectorAll(".header-icon-svg");
+    let headerShoppingBagCounter = document.querySelector('.header-shopping-bag-quantity-notification-span');
 
     // Check if the device supports touch events
     var isTouchDevice = 'ontouchstart' in document.documentElement;
@@ -38,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
             headerIcons.forEach(function (icon) {
                 icon.classList.add("on-header-hover");
             });
+            headerShoppingBagCounter.classList.add('on-header-hover');
 
             // Change color of anchor elements inside the header
             for (var i = 0; i < anchorElements.length; i++) {
@@ -57,6 +62,7 @@ document.addEventListener("DOMContentLoaded", function () {
             headerIcons.forEach(function (icon) {
                 icon.classList.remove("on-header-hover");
             });
+            headerShoppingBagCounter.classList.remove('on-header-hover');
 
             // Reset anchor element colors
             for (var i = 0; i < anchorElements.length; i++) {
@@ -70,7 +76,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-// END HEADER APPEARANCE ON HOVER
+// END HEADER APPEARANCE ON HOVER on computers
 
 // BEGIN MENU (On click) SCRIPT
 function menuOnClick() {
@@ -78,12 +84,18 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change-nav");
     document.getElementById("menu-bg").classList.toggle("change-bg");
     document.getElementById("myHeader").classList.toggle("on-menu-click-header");
+    document.querySelector('.header-hamburger-menu-white-strip-div-for-computer').classList.toggle('after-clicking-on-hamburger-menu-icon-to-open-hamburger-menu');
+    document.querySelector('.menu-bg-inside-bg-div').classList.toggle('after-clicking-on-hamburger-menu-icon-in-header');
 
-    // Iterate over each header-icon-img element and toggle the class
-    var headerIcons = document.querySelectorAll(".header-icon-img");
+    // Iterate over each header-icon-svg element and toggle the class
+    var headerIcons = document.querySelectorAll(".header-icon-svg");
     headerIcons.forEach(function (icon) {
         icon.classList.toggle("on-menu-click-header-icon");
     });
+
+    // Toggle the class on the header shopping bag counter
+    let headerShoppingBagCounter = document.querySelector('.header-shopping-bag-quantity-notification-span');
+    headerShoppingBagCounter.classList.toggle('on-menu-click');
 
     // Toggle menu-opened class on body
     document.body.classList.toggle("menu-opened");
@@ -99,6 +111,7 @@ function emailUsOnClick() {
 }
 // END EMAIL US (On click)
 
+// BEGIN CALCULATE HEADER HEIGHT AND MAKE MAIN ELEMENT BELOW THAT
 //MAIN ELEMENT TOP MARGIN = HEADER HEIGHT (FOR NON-INDEX-PAGES)
 document.addEventListener('DOMContentLoaded', function () {
     var nonIndexHeader = document.querySelector('.non-index-header');
@@ -145,7 +158,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
+// BEGIN PRODUCT CARD IMAGE BUTTONS (LEFT AND RIGHT)
 document.querySelectorAll('.product-card-div').forEach((card, index) => {
     const container = card.querySelector('.product-card-img-container-div');
     const leftBtn = card.querySelector('.product-card-left-chevron-button');
@@ -179,3 +192,105 @@ document.querySelectorAll('.product-card-div').forEach((card, index) => {
         }
     });
 });
+// END PRODUCT CARD IMAGE BUTTONS (LEFT AND RIGHT)
+
+/* BEGIN SHOPPING BAG */
+// BEGIN ADD 1 EVERY TIME YOU PRESS PLACE IN CART
+// Function to add 1 to the shopping cart variable
+function placeInBag() {
+    // Retrieve the bagQuantity from localStorage, or default to 0 if it doesn't exist
+    let bagQuantity = parseInt(localStorage.getItem('bagQuantity')) || 0;
+
+    bagQuantity++;
+    // Store the updated bagQuantity in localStorage
+    localStorage.setItem('bagQuantity', bagQuantity);
+
+    document.querySelector('.header-shopping-bag-quantity-notification-span').innerHTML = bagQuantity;
+    document.querySelector('.shopping-bag-menu-on-click-title-h1-span-quantity').innerHTML = bagQuantity;
+}
+// END ADD 1 EVERY TIME YOU PRESS PLACE IN CART
+
+// BEGIN UPDATE .header-shopping-bag-quantity-notification-span WITH bagQuantity from localStorage on page load
+window.onload = function () {
+    // Retrieve the bagQuantity from localStorage, or default to 0 if it doesn't exist
+    let bagQuantity = parseInt(localStorage.getItem('bagQuantity')) || 0;
+
+    // Set the inner HTML of the element to the bagQuantity
+    document.querySelector('.header-shopping-bag-quantity-notification-span').innerHTML = bagQuantity;
+    document.querySelector('.shopping-bag-menu-on-click-title-h1-span-quantity').innerHTML = bagQuantity;
+}
+// END UPDATE .header-shopping-bag-quantity-notification-span WITH bagQuantity from localStorage on page load
+
+//BEGIN OPEN SHOPPING BAG MENU ON CLICKING SHOPPING BAG ICON IN HEADER
+document.querySelector('.shopping-bag-icon-container-div').addEventListener('click', function () {
+    document.querySelector('.shopping-bag-white-menu-half-div-for-computer').classList.add('after-clicking-on-shopping-bag-icon');
+    document.querySelector('.shopping-bag-in-page-menu-div-for-computer').classList.add('after-clicking-on-shopping-bag-icon');
+    document.querySelector('.shopping-bag-dark-menu-half-div-for-computer').classList.add('after-clicking-on-shopping-bag-icon');
+    // Begin keep header changed when shopping bag menu is open (not only one hover): open header classes
+    document.getElementById("myHeader").classList.add("on-menu-click-header");
+
+    // Iterate over each header-icon-svg element and toggle the class
+    var headerIcons = document.querySelectorAll(".header-icon-svg");
+    headerIcons.forEach(function (icon) {
+        icon.classList.add("on-menu-click-header-icon");
+    });
+
+    // Toggle the class on the header shopping bag counter
+    let headerShoppingBagCounter = document.querySelector('.header-shopping-bag-quantity-notification-span');
+    headerShoppingBagCounter.classList.add('on-menu-click');
+
+    // End keep header changed when shopping bag menu is open (not only one hover): open header classes
+});
+
+//END OPEN SHOPPING BAG MENU ON CLICKING SHOPPING BAG ICON IN HEADER
+// BEGIN CLOSE SHOPPING BAG ICON ON CLICK either the X or by clicking outisde the shopping bag (clicking the dark half)
+// Function to remove the class and trigger mouseout on the header
+function removeClassesAndTriggerMouseoutOnShoppingBagMenuClose(event) {
+    // Select the elements
+    const closeButton = document.querySelector('.close-shopping-bag-in-page-menu-div-button-for-computer');
+    const darkMenu = document.querySelector('.shopping-bag-dark-menu-half-div-for-computer');
+    const whiteMenu = document.querySelector('.shopping-bag-white-menu-half-div-for-computer');
+    const header = document.getElementById('myHeader');
+    // Begin keep header changed when shopping bag menu is open (not only one hover): close header classes
+    // because this function is for when the shopping bag menu is closed
+    document.getElementById("myHeader").classList.remove("on-menu-click-header");
+
+    // Iterate over each header-icon-svg element and toggle the class
+    var headerIcons = document.querySelectorAll(".header-icon-svg");
+    headerIcons.forEach(function (icon) {
+        icon.classList.remove("on-menu-click-header-icon");
+    });
+
+    // Toggle the class on the header shopping bag counter
+    let headerShoppingBagCounter = document.querySelector('.header-shopping-bag-quantity-notification-span');
+    headerShoppingBagCounter.classList.remove('on-menu-click');
+
+    // End keep header changed when shopping bag menu is open (not only one hover): close header classes
+    // because this function is for when the shopping bag menu is closed
+
+    // Check if the mouse cursor is below the header offset height
+    const mouseY = event.clientY || event.touches[0].clientY;
+    const headerHeight = header.offsetHeight;
+
+    // Remove classes on shopping bag menu close
+    whiteMenu.classList.remove('after-clicking-on-shopping-bag-icon');
+    darkMenu.classList.remove('after-clicking-on-shopping-bag-icon');
+
+
+
+    // if mouse cursor is under the header offset height whatever that means then trigger a mouseout event as if the user just left the header
+    if (mouseY > headerHeight) {
+        // Trigger mouseout on the header
+        const mouseoutEvent = new Event('mouseout');
+        header.dispatchEvent(mouseoutEvent);
+    }
+}
+
+// Add the event listeners
+document.querySelector('.close-shopping-bag-in-page-menu-div-button-for-computer').addEventListener('click', removeClassesAndTriggerMouseoutOnShoppingBagMenuClose);
+document.querySelector('.shopping-bag-dark-menu-half-div-for-computer').addEventListener('click', removeClassesAndTriggerMouseoutOnShoppingBagMenuClose);
+
+
+
+// END CLOSE SHOPPING BAG ICON ON CLICK either the X or by clicking outisde the shopping bag (clicking the dark half)
+/* END SHOPPING BAG */
